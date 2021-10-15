df17abde0985d2ecb29b982d8394654ad222d300
ingframin <framin2@gmail.com>
2021-10-15 16:50:48 +0200

    Added roll 20 mechanic

--- src/mutant_chronicles/dices.py
+++ src/mutant_chronicles/dices.py
@@ -30,6 +30,25 @@ def roll(n,d):
 def sum_roll(n,d):
     return sum([d() for x in range(n)])
 
+def rolld20(target, n_dices, focus = 0, difficulty = 1):
+    result = roll(n_dices,d20)
+    successes = 0
+    complications = 0
+    for r in result:
+        if r <= focus:
+                successes += focus
+        
+        elif r <= target:
+            successes += 1
+            
+        if r == 20:
+            complications += 1
+
+    momentum = successes - difficulty
+    if momentum >= 0:
+        return ('success',successes, momentum, complications)
+    
+    return ('failure', successes, 0, complications) 
 
 
         

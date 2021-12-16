ce2cacd6717d8cb383b43db434d1953c8e349816
ingframin <framin2@gmail.com>
2021-12-16 17:37:49 +0100

    Official begin

--- src/common/dices.py
+++ src/common/dices.py
@@ -1,28 +1,13 @@
 from random import randint
 
-def d2():
-    return randint(1,2)
-
-def d4():
-    return randint(1,4)
-
 def d6():
     return randint(1,6)
-
-def d8():
-    return randint(1,8)
-
-def d10():
-    return randint(1,10)
 
 def d12():
     return randint(1,12)
 
 def d20():
     return randint(1,20)
-
-def d100():
-    return randint(1,100)
 
 def roll(n,d):
     return [d() for x in range(n)]
@@ -31,9 +16,11 @@ def sum_roll(n,d):
     return sum([d() for x in range(n)])
 
 def rolld20(target, n_dices, focus = 0, difficulty = 1):
+    
     result = roll(n_dices,d20)
     successes = 0
     complications = 0
+    
     for r in result:
         if r <= focus:
                 successes += focus
@@ -45,11 +32,13 @@ def rolld20(target, n_dices, focus = 0, difficulty = 1):
             complications += 1
 
     momentum = successes - difficulty
-    if momentum >= 0:
-        return ('success',successes, momentum, complications)
-    
-    return ('failure', successes, 0, complications) 
+    succeed=True
+    if momentum < 0:
+        momentum = 0
+        succeed = False
 
+    return (succeed,successes, momentum, complications)
+    
 
         
 

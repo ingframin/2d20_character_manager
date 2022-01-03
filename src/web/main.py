88fcd5921011969c09fcfe25f07ae16881307412
ingframin <framin2@gmail.com>
2022-01-03 23:23:44 +0100

    Fixed cherrypy configuration

--- src/web/main.py
+++ src/web/main.py
@@ -1,10 +1,25 @@
 import cherrypy
+from mako.template import Template
+import os
+
 """
 This is just a stub
 """
 class Start:
     @cherrypy.expose
     def index(self):
-        return "Hello World"
+        home = Template(filename='./templates/begin.html')
+        return home.render()
 
-cherrypy.quickstart(Start())
+
+conf = {
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+        },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './templates'
+        },
+}
+cherrypy.quickstart(Start(),'/',conf)

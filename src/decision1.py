922abc6f860c33ccb22dc59f22c31299096d9bbc
ingframin <framin2@gmail.com>
2023-04-17 11:14:39 +0200

    -

---
+++ src/decision1.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+class Decision1:
+
+    def __init__(self, master) -> None:
+        self.frame = tk.Frame(master,width=800,height=500)
+        self.frame.pack()
+
+
+    

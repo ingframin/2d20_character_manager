0b90970e9bcaa1c65b5ddbba5c500a166f59985a
Franco Minucci <framin2@gmail.com>
2023-06-14 11:07:33 +0200

    -

--- src/flapp.py
+++ src/flapp.py
@@ -7,7 +7,6 @@ import json
 app = Flask(__name__,template_folder='templates',static_folder='assets')
 app.config["DATA_DIR"] = os.path.join(os.getcwd(), "tables")
 
-character = {}
 
 @app.route("/")
 def home():
@@ -15,57 +14,7 @@ def home():
 
 @app.route("/standard_generation", methods=['GET','POST'])
 def standard_generation():
-    attributes = load_attribs('./tables/attributes.json')
-    if request.method == 'POST':
-        new_attribs = request.form.to_dict()
-        checksum = 0
-        for a in new_attribs:
-            
-            if int(new_attribs[a]) < 4 or int(new_attribs[a]) > 6:
-                return redirect(url_for("standard_generation",error="Attributes should be between 4 and 6"))
-            for atr in attributes:
-                if atr.name == a:
-                    atr.value = int(new_attribs[a])
-            checksum += int(new_attribs[a])
-        if checksum != 40:
-            
-            return redirect(url_for("standard_generation",error="The sum of all attributes should be 40!"))
-        
-        character['attribs'] = attributes
-        return redirect(url_for("heritage_faction"))
-        
-    else:         
-        try:
-            ercode = f"Error: {request.args['error'] }"
-        except:
-            ercode = ''
-        # Render template with attributes and initial values
-        return render_template('start_attributes.html', attributes=attributes, error=ercode)
-
-
-
-@app.route("/heritage_faction",methods=['GET','POST'])
-def heritage_faction():
-
-    fac_herit = load_factions('./tables/factions_heritage.json')
-    if 'faction' not in character.keys() or 'heritage' not in character.keys():
-        faction, heritage = faction_heritage(fac_herit)
-        character['faction'] = faction
-        character['heritage'] = heritage
-    else:
-        faction = character['faction']
-        heritage = character['heritage']
-    
-    talent,languages,skills = fac_talents_lang_skills(fac_herit, faction, heritage)
-    character['talents'] = [talent]
-    print(talent, languages, skills)
-    if len(skills)>2:
-        print("you can only keep 2 skills and select one as signature")
-    
-    event=load_fact_event('./tables/faction_events.json', heritage)    
-    print(event)
-    return render_template('faction_heritage.html',faction = faction, heritage = heritage, skills=skills)
-
+    return "Standard Generation"
 
 
 @app.route("/points_system")
@@ -79,10 +28,13 @@ def free_character():
     return "Free Character"
 
 if __name__ == "__main__":
+    # app.run(debug=True)
+
     import webview
     from threading import Thread
     apptr = Thread(target=app.run, kwargs={'debug':False})
     apptr.start()
-    webview.create_window('Hello world', 'http://127.0.0.1:5000',width=1000,height=650)
+    webview.create_window('Hello world', 'http://127.0.0.1:5000/',width=1000,height=650)
     webview.start()
     apptr.join()
+    exit(0)       

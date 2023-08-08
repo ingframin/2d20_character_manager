d1929a78d5f284c3e17bf2e8f5afbba87b184027
ingframin <framin2@gmail.com>
2023-08-08 10:26:46 +0200

    Integrated attrib functionality in decisions.py

--- src/decisions.py
+++ src/decisions.py
@@ -1,4 +1,4 @@
-from data_model import Attribute,Faction
+from data_model import Attribute,Faction, Skill
 from attribs import load_attribs,validate_attribs
 import json
 from abc import ABC
@@ -9,8 +9,27 @@ class Decision(ABC):
 class DecisionOneAttribs(Decision):
     def __init__(self,life_points:int=5, attrib_file:str='./tables/attributes.json') -> None:
         self.LP = life_points
-        self.attributes = load_attribs(attrib_file)
+        self.attributes = self.load_attribs(attrib_file)
     
+    @staticmethod
+    def load_attribs(filename:str)->list[Attribute]:
+        with open(filename) as fp:
+            attribs = json.load(fp)
+        attributes = []
+
+        for a in attribs:
+            print(a)
+            atr = Attribute(a['name'],a['value'],[])
+            for sn in a['skills']:
+                atr.skills.append(Skill(sn))
+            attributes.append(atr)
+        return attributes
+    
+    @staticmethod
+    def validate_attribs(new_attribs:list[Attribute], target:int, minv:int = 4, maxv:int = 6,)->bool:
+        vals = [a.value for a in new_attribs]
+        return sum(vals) == target and all([a.validate(minv,maxv) for a in new_attribs])
+
     def adjust_attrib(self,name:str,new_value:int, max_LP:int=5, target:int=40):
         if new_value < 4 or new_value > 6:
             raise ValueError("Attribute values must be between 4 and 6")
@@ -33,7 +52,8 @@ class DecisionTwoFactionHeritage(Decision):
         # This should be another function!!
         self.facts_herit = self.load_factions(fact_file)
     
-    def load_factions(self,filename:str)->list[Faction]:
+    @staticmethod
+    def load_factions(filename:str)->list[Faction]:
         with open(filename) as fp:
             facts_table = json.load(fp)
         facts = []

e00bf35c2faef9a9575267e2dbc24c3eed2e387a
framin2@gmail.com <framin2@gmail.com>
2024-12-27 22:47:46 +0100

    started encoding data types

--- MC3CharacterGenerator/src/Library/Library.fs
+++ MC3CharacterGenerator/src/Library/Library.fs
@@ -2,6 +2,13 @@
 
 open System.Text.Json
 
+type AttributeName = | Strength | Physique | Agility | Awareness | Coordination | Intelligence | Mental | Personality
+
+type Career = {roll: int; career: string; attribs: Map<AttributeName, int>; mandatory_skills: string list; elective_skills: string list; talents: string list; equipment: string list}
+
+type Attribute = {name: AttributeName; value: int} 
+type Skill = {name:string; expertise: int; focus: int; parent:AttributeName}
+
 
 let getJson value =
     let json = JsonSerializer.Serialize(value)

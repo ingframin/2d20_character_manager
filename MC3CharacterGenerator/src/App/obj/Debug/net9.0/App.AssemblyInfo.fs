7c1a6fede13696351fd44f59f28ff001d48f6d78
framin2@gmail.com <framin2@gmail.com>
2024-12-29 11:56:37 +0100

    -

--- MC3CharacterGenerator/src/App/obj/Debug/net9.0/App.AssemblyInfo.fs
+++ MC3CharacterGenerator/src/App/obj/Debug/net9.0/App.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("App")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0513190544a6559e990f8eca0306db2587dd97f8")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e00bf35c2faef9a9575267e2dbc24c3eed2e387a")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("App")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("App")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
